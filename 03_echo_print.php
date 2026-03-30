71d53e63ee270086123d9b40b7c29ee4585ed745
maruf-git <munna.csehstu@gmail.com>
2026-03-30 11:59:10 +0600

    echo and print

---
+++ 03_echo_print.php
@@ -0,0 +1,23 @@
+<?php
+
+// PHP echo and print
+// With PHP, there are two basic ways to output data to the screen: echo and print.
+
+// The differences are small:
+
+// echo has no return value, while print has a return value of 1 so it can be used in expressions
+// echo can take multiple parameters, while print can take only one argument
+// echo is marginally faster than print 
+
+
+// echo
+echo "hello world";
+echo ("hi world");
+
+echo ("<h1>Hello heading <h1> <br>");
+echo "<h3>I am about to learn php<h3>";
+
+
+// print
+print "Hello Bangladesh";
+print("Hello None");
