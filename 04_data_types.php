8e271ef56254de9e2ab92dad48c50bd3539b58db
maruf-git <munna.csehstu@gmail.com>
2026-03-30 12:15:01 +0600

    data types

---
+++ 04_data_types.php
@@ -0,0 +1,25 @@
+<?php
+
+// PHP supports the following data types:
+// string (text values)
+// int (whole numbers)
+// float (decimal numbers)
+// bool (true or false)
+// array (multiple values)
+// object (stores data as objects)
+// null (empty variable)
+// resource (references external resources)
+
+// Use var_dump() to Get the Data Type
+
+
+// array
+$cars = array("volvo", "bmw", "toyota");
+// echo "data type of car is: $var_dump($cars)";
+print_r($cars);
+
+
+echo "data type of car is: <br>";
+var_dump($cars);
+echo "<br>carys array: ";
+print_r($cars);
